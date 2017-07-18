f638f42a0ba6fc49766a8efa4319c14e82a2e9f3
Dymsi <30205360+Dymsi@users.noreply.github.com>
2017-07-18 23:32:48 +0100

    Organized Savage_Program class, new Savage_Commands.
    Savage_Program class:
     -Added #region to most code blocks for better organization.
     -Control variable added (bool Is_Win_7), to allow execution of server on both Windows 7 and Windows 10.
     -Added Program Log method, to log anything that happens on server, and output to server console.
     -Added Json Loader method to load configs and variables from .json files (not used anywhere because json is a bitch. Will be implemented later).
    
    Savage_Commands class:
     -Completely remade from scratch.
     -Added Command Log method to log commands request and output to server console.
     -Added Program Log method to log command output and output to server console.
     -Added <Show Commands> and <Credits> commands, to show available commands and get the credits card, respectively.
     -Added <GetInfo> command, to aid on the development of other commands.
    
    Notes:
     -Every command created now should use the [Hidden] tag, has we now use our own Show Commands method.
     -Show C

---
+++ SavageCommands_Version2_Awating_Ver.cs
@@ -0,0 +1,114 @@
+﻿#region Imports
+using System;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Text;
+using System.Windows;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using System.IO;
+using System.Management;
+#endregion Imports
+
+namespace Savage_Bot
+{
+    class Commands_Base
+    {
+        #region Command Logs
+        public static void Log(CommandContext ctx)
+        {
+            string log_time = "[" + DateTime.Now + "][Client] Command <" + ctx.Command.Name.ToString() + "> from group [Dev] issued by " + ctx.Member.DisplayName.ToString() + " in guild " + ctx.Guild.Name.ToString() + ".";
+            Console.WriteLine(log_time);
+        }
+        #endregion Command Logs
+
+        #region Program Logs
+        public void C_Log(string source, string level, string log)
+        {
+            string log_time = "[" + DateTime.Now + "]";
+            string log_source = "[" + source + "] ";
+            if (level == "Warning" || level == "warning")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (level == "Critical" || level == "critical")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine(log_time + log_source + log);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+        #endregion Program Logs
+
+        #region Show Commands
+        [Command("Commands")]
+        [Aliases("commands")]
+        [Description("Show currently available commands.")]
+        public async Task Base_Help(CommandContext ctx)
+        {
+            Log(ctx);
+            await ctx.TriggerTypingAsync();
+            DiscordEmbed member = new DiscordEmbed();
+            member.Color = 11564709;
+            member.Title = " - Commands";
+            member.Description = "Shows all the available commands";
+            member.Thumbnail = new DiscordEmbedThumbnail { Url = "http://i2.kym-cdn.com/entries/icons/original/000/020/309/nice.jpg" };
+            member.Author = new DiscordEmbedAuthor { Name = "Available Commands: ", IconUrl = "http://i2.kym-cdn.com/entries/icons/original/000/020/309/nice.jpg" };
+            member.Footer = new DiscordEmbedFooter { Text = "Made by Savage Bot.", IconUrl = "https://cdn.discordapp.com/embed/avatars/0.png" };            
+            await ctx.RespondAsync("", false, member);
+            C_Log("Output", "Info", "Available Commands: ");
+            C_Log("Output", "Info", " - Commands");
+            C_Log("Output", "Info", " Shows all the available commands");
+            C_Log("Output", "Info", "Made by Savage Bot.");
+
+        }
+        #endregion Show Commands
+
+        [Command("Credits")]
+        [Aliases("credits")]
+        [Hidden]
+        [Description("Shows info about source code and developers.")]
+        public async Task Base_Credits(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            DiscordEmbed member = new DiscordEmbed();
+            member.Color = 11564709;
+            member.Title = "Developed by: ";
+            member.Description = "<@!212231937039794176>";
+            member.Thumbnail = new DiscordEmbedThumbnail { Url = "https://cdn.discordapp.com/avatars/212231937039794176/fd5962d4dbba1a1f6b15349575e0ddda.png?size=1024" };
+            member.Fields.Add(new DiscordEmbedField { Name = "Source Code: ", Value = "[GitHub](https://github.com/RikMachado/SavageBot)" });
+            member.Fields.Add(new DiscordEmbedField { Name = "Library used: ", Value = "[DSharpPlus](https://github.com/NaamloosDT/DSharpPlus/)" });
+            member.Author = new DiscordEmbedAuthor { Name = "Credits" };
+            member.Footer = new DiscordEmbedFooter { Text = "Made by Savage Bot", IconUrl = "https://cdn.discordapp.com/embed/avatars/0.png" };
+            await ctx.RespondAsync("", false, member);
+        }
+
+        [Command("GetInfo")]
+        [Aliases("getinfo")]
+        [Hidden]
+        [Description("Debug test command. Change for whatever needed.")]
+        public async Task Base_Test(CommandContext ctx, DiscordMember memb)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("```Display name: " + memb.DisplayName + ",\n" +
+                                   "User name: " + memb.Username + ",\n" +
+                                   "4 digit ID: " + memb.Discriminator + ",\n" +
+                                   "Full ID: " + memb.Id.ToString() + ",\n" +
+                                   "Mention Code: " + memb.Mention + ",\n" +
+                                   "Avatar URL: " + memb.AvatarUrl + ".```");
+        }
+    }
+}
---
+++ SavageProgram_Version2_Awating_Ver.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.CommandsNext.Attributes;
+using Newtonsoft.Json;
+
+
+namespace Savage_Bot
+{
+    class Program
+    {
+        #region Vars and Objects
+        public bool Is_Win_7 = false;
+
+        public string Savage_Config_Path = "D:\\C#\\Savage Bot\\Alpha_0_1\\SavageCommands\\SavageCommands\\Configs\\Savage_Config.json";
+
+        public DiscordClient Client { get; set; }
+
+        public CommandsNextModule Commands { get; set; }
+
+
+        #endregion Vars and Objects
+
+        #region Main
+        static void Main(string[] args)
+        {
+
+            var prog = new Program();
+            prog.Run().GetAwaiter().GetResult();
+        }
+        #endregion Main
+
+        #region Json Loader
+        public static T ReadFromJsonFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(filePath);
+                var fileContents = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+        #endregion Json Loader
+
+        #region Program Logs
+        public void Log(string source, string level, string log)
+        {
+            string log_time = "[" + DateTime.Now + "]";
+            string log_source = "[" + source + "]";
+            if (level == "Warning" || level == "warning")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (level == "Critical" || level == "critical")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine(log_time + log_source + log);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+        #endregion Program Logs
+
+        #region Running System
+        public async Task Run()
+        {
+            Log("Server", "Critical", "Savage bot is starting.");
+
+            #region Misc Variables
+            Log("Server", "Critical", "Initializing misc. variables.");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;           
+            Console.Title = "Savage Bot";
+            Log("Server", "Critical", "Variables initialized.");
+            #endregion Misc Variables
+
+            #region Client Config setup
+            Log("Server", "Critical", "Setting up client config.");
+            DiscordConfig savage_config = new DiscordConfig
+            {
+                AutoReconnect = true,
+                DiscordBranch = Branch.Stable,
+                LargeThreshold = 250,
+                LogLevel = LogLevel.Debug,
+                Token = "MzM1MDg3ODgyNTM0Mzg3NzEy.DElJDw.RSeek-v_OL9Q0DEsJuvMMpixMv8",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = false
+            };          
+            Log("Server", "Critical", "Loading configs.");            
+            this.Client = new DiscordClient(savage_config);
+            Log("Server", "Critical", "Configs loaded.");
+            #endregion Client Config setup
+
+            #region Command Configs and Prefix
+            Log("Server", "Critical", "Setting up Commands config and prefix.");
+            var CommandConfig = new CommandsNextConfiguration
+            {
+                StringPrefix = "#",
+
+                EnableMentionPrefix = true,
+
+                EnableDms = false
+            };
+            this.Commands = this.Client.UseCommandsNext(CommandConfig);
+            Log("Server", "Critical", "Commands config loaded.");
+            #endregion Command Configs and Prefix
+
+            #region Command Loader
+            Log("Server", "Critical", "Loading command classes.");
+            this.Commands.RegisterCommands<Commands_Base>();
+            Log("Server", "Critical", "Command classes loaded.");
+            #endregion Command Loader
+
+            #region Bot Connection and Task Looping
+            Log("Server", "Critical", "Starting bot connection.");
+            var discord_conn = new DiscordConnection();
+            if (Is_Win_7 == true)
+            {
+                this.Client.SetWebSocketClient<WebSocket4NetClient>();
+            }
+            await this.Client.ConnectAsync();
+            Log("Server", "Critical", "Bot connected.");
+            Log("Server", "Info", "Enjoy your stay.");
+            await Task.Delay(-1);
+            #endregion Bot Connection and Task Looping
+        }
+        #endregion Running System
+    }
+}
