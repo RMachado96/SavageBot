abf31a7555bb76b8490891b5056a3328943a8f71
Dymsi <30205360+Dymsi@users.noreply.github.com>
2017-07-19 01:28:24 +0100

    Added all output to console needed. New method to save and print command name and description.
    Added json loader to load .json config files.
    Command names and descriptions are now saved on a json file, for better organization.
    Command <Commands> now runs on a foreach loop, which makes it much simpler, and no need to update it.
    Commands info can be both added through the command <new_c> and directly into the file.
    Added "Dictionary_Path", variable that should contain the path to the file Command_Dictionary.json.
    Command_Dictionary.json is mandatory to exist, so I'll upload it to the repository too. Simply download it, and copy its path to the variable.
    Currently there is no method to check the existence of this file.

--- SavageCommands_Version2_Awating_Ver.cs
+++ SavageCommands_Version2_Awating_Ver.cs
@@ -1,4 +1,4 @@
-﻿#region Imports
+#region Imports
 using System;
 using System.Net.NetworkInformation;
 using System.Linq;
@@ -14,17 +14,22 @@ using System.CodeDom.Compiler;
 using Microsoft.CSharp;
 using System.IO;
 using System.Management;
+using Newtonsoft.Json;
 #endregion Imports
 
 namespace Savage_Bot
 {
     class Commands_Base
     {
+        public string Dictionary_Path = "D:\\C#\\Savage Bot\\Alpha_0_1\\SavageCommands\\SavageCommands\\Configs\\Command_Dictionary.json";
+
         #region Command Logs
         public static void Log(CommandContext ctx)
         {
+            Console.ForegroundColor = ConsoleColor.White;
             string log_time = "[" + DateTime.Now + "][Client] Command <" + ctx.Command.Name.ToString() + "> from group [Dev] issued by " + ctx.Member.DisplayName.ToString() + " in guild " + ctx.Guild.Name.ToString() + ".";
             Console.WriteLine(log_time);
+            Console.ForegroundColor = ConsoleColor.Blue;
         }
         #endregion Command Logs
 
@@ -53,6 +58,24 @@ namespace Savage_Bot
         }
         #endregion Program Logs
 
+        #region Json Loader
+        public static T ReadFromJsonFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(filePath);
+                var fileContents = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+        #endregion Json Loader
+
         #region Show Commands
         [Command("Commands")]
         [Aliases("commands")]
@@ -63,45 +86,53 @@ namespace Savage_Bot
             await ctx.TriggerTypingAsync();
             DiscordEmbed member = new DiscordEmbed();
             member.Color = 11564709;
-            member.Title = " - Commands";
-            member.Description = "Shows all the available commands";
-            member.Thumbnail = new DiscordEmbedThumbnail { Url = "http://i2.kym-cdn.com/entries/icons/original/000/020/309/nice.jpg" };
-            member.Author = new DiscordEmbedAuthor { Name = "Available Commands: ", IconUrl = "http://i2.kym-cdn.com/entries/icons/original/000/020/309/nice.jpg" };
-            member.Footer = new DiscordEmbedFooter { Text = "Made by Savage Bot.", IconUrl = "https://cdn.discordapp.com/embed/avatars/0.png" };            
-            await ctx.RespondAsync("", false, member);
+            member.Thumbnail = new DiscordEmbedThumbnail { Url = "http://www.dndjunkie.com/civilopedia/images/large/RESOURCE_BANANA.png" };
+            member.Author = new DiscordEmbedAuthor { Name = "Available Commands: ", IconUrl = "http://www.dndjunkie.com/civilopedia/images/large/RESOURCE_BANANA.png" };
+            member.Footer = new DiscordEmbedFooter { Text = "Made by Savage Bot.", IconUrl = "https://cdn.discordapp.com/embed/avatars/0.png" };
+            Dictionary<string, string> Command_Dict = new Dictionary<string, string>();
+            Command_Dict = ReadFromJsonFile<Dictionary<string, string>>(Dictionary_Path);
             C_Log("Output", "Info", "Available Commands: ");
-            C_Log("Output", "Info", " - Commands");
-            C_Log("Output", "Info", " Shows all the available commands");
-            C_Log("Output", "Info", "Made by Savage Bot.");
-
+            foreach (var item in Command_Dict)
+            {
+                member.Fields.Add(new DiscordEmbedField { Name = item.Key, Value = item.Value });
+                C_Log("Output", "Info", " -" + item.Key);
+                C_Log("Output", "Info", " " + item.Value);
+            }
+            await ctx.RespondAsync("", false, member);
         }
         #endregion Show Commands
 
+        #region Credits
         [Command("Credits")]
         [Aliases("credits")]
         [Hidden]
         [Description("Shows info about source code and developers.")]
         public async Task Base_Credits(CommandContext ctx)
         {
+            Log(ctx);
             await ctx.TriggerTypingAsync();
             DiscordEmbed member = new DiscordEmbed();
             member.Color = 11564709;
             member.Title = "Developed by: ";
-            member.Description = "<@!212231937039794176>";
+            member.Description = "<@!212231937039794176> \n<@!110141872948645888>";
             member.Thumbnail = new DiscordEmbedThumbnail { Url = "https://cdn.discordapp.com/avatars/212231937039794176/fd5962d4dbba1a1f6b15349575e0ddda.png?size=1024" };
             member.Fields.Add(new DiscordEmbedField { Name = "Source Code: ", Value = "[GitHub](https://github.com/RikMachado/SavageBot)" });
             member.Fields.Add(new DiscordEmbedField { Name = "Library used: ", Value = "[DSharpPlus](https://github.com/NaamloosDT/DSharpPlus/)" });
             member.Author = new DiscordEmbedAuthor { Name = "Credits" };
             member.Footer = new DiscordEmbedFooter { Text = "Made by Savage Bot", IconUrl = "https://cdn.discordapp.com/embed/avatars/0.png" };
             await ctx.RespondAsync("", false, member);
+            C_Log("Output", "Info", "Showing Credits card.");
         }
+        #endregion Credits       
 
+        #region Get info tool for bot development
         [Command("GetInfo")]
         [Aliases("getinfo")]
         [Hidden]
         [Description("Debug test command. Change for whatever needed.")]
         public async Task Base_Test(CommandContext ctx, DiscordMember memb)
         {
+            Log(ctx);
             await ctx.TriggerTypingAsync();
             await ctx.RespondAsync("```Display name: " + memb.DisplayName + ",\n" +
                                    "User name: " + memb.Username + ",\n" +
@@ -109,6 +140,38 @@ namespace Savage_Bot
                                    "Full ID: " + memb.Id.ToString() + ",\n" +
                                    "Mention Code: " + memb.Mention + ",\n" +
                                    "Avatar URL: " + memb.AvatarUrl + ".```");
+
+            C_Log("Output", "Info", "Display name: " + memb.DisplayName + ".");
+            C_Log("Output", "Info", "User name: " + memb.Username + ".");
+            C_Log("Output", "Info", "4 digit ID: " + memb.Discriminator + ".");
+            C_Log("Output", "Info", "Full ID: " + memb.Id.ToString() + ".");
+            C_Log("Output", "Info", "Mention Code: " + memb.Mention + ".");
+            C_Log("Output", "Info", "Avatar URL: " + memb.AvatarUrl + ".");
         }
+        #endregion Get info tool for bot development
+
+        //Commands can be added through here and the file itself. This command will eventually be deleted.
+        //Every command can be fully edited on the .json file.
+        #region Add command to dict
+        [Command("new_c")]
+        [Description("Create a new command")]
+        public async Task test(CommandContext ctx, [Description("Command name")] string command_name, [Description("Command result")] [RemainingText] string command_func)
+        {
+            Log(ctx);
+            await ctx.TriggerTypingAsync();
+            Dictionary<string, string> Command_Dict = new Dictionary<string, string>();
+            Command_Dict = ReadFromJsonFile<Dictionary<string, string>>(Dictionary_Path);
+            Command_Dict.Add(command_name, command_func);
+            await ctx.RespondAsync("Command <" + command_name + "> with the description: \"" + command_func + "\" was added.");
+            C_Log("Output", "Info", "Command <" + command_name + "> with the description: \"" + command_func + "\" was added.");
+            #region Save to Json
+            var json_obj_list = JsonConvert.SerializeObject(Command_Dict);
+            TextWriter writer_obj = new StreamWriter("D:\\C#\\Savage Bot\\Alpha_0_1\\SavageCommands\\SavageCommands\\Configs\\Command_Dictionary.json", false);
+            writer_obj.Write(json_obj_list);
+            writer_obj.Close();
+            #endregion Save to Json
+
+        }
+        #endregion Add command to dict
     }
 }
